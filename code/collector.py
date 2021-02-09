be724fe84c5b0e5788a409e7d35d756eafa0d0b3
Daniel <raptersong@gmail.com>
2021-02-08 20:39:33 -0500

    modified collector, now functional

--- code/collector.py
+++ code/collector.py
@@ -11,22 +11,21 @@ import csv
 
 
 
-""" #TODO utilize and test upon api key activation
+ #TODO utilize and test upon api key activation
 
 
 fieldNames=['adherence','block_abbr','block_id','direction','last_updated','latitude','longitude','route','stop_id'
     ,'timepoint','trip_id','vehicle']
 
+
 dataDict = get_buses()
 
-fields = [dataDict[x] for x in fieldnames]
-
-"""
-
-#fields = ["hi","this","test","qoeka"]
+print(dataDict[0])
 
 
 with open(r'liveData.csv', 'a',newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(fields)
+    for row in dataDict:
+        fields = [getattr(row,x) for x in fieldNames]
+        writer = csv.writer(f)
+        writer.writerow(fields)
     
