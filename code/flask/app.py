ec91e0125f16c5bee02f45f236d21e917a2b3fe8
daniel-redder <raptersong@gmail.com>
2021-03-02 21:58:43 -0500

    added template homepage

--- code/flask/app.py
+++ code/flask/app.py
@@ -5,3 +5,20 @@ Created on Tue Mar  2 21:34:47 2021
 @author: rapte
 """
 
+from flask import Flask, render_template, request, redirect, url_for, flash
+import json
+import os.path
+
+
+app = Flask(__name__, template_folder='templates')
+
+#Flask secret key TODO set as env
+app.secret_key = 'asdgagaweawsfasdfaqw'
+
+
+#home page --initial template adapted from w3schools css tutorial
+@app.route('/')
+def home_builder():
+      return render_template("home.html")
+
+app.run()
